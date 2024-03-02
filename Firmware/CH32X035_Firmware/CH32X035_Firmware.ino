1733fa7caf8048d30c17ef2249b77acf58369d0c
Winnie Szeto <87886980+wszeto9@users.noreply.github.com>
2024-03-02 15:57:46 -0500

    Added automatic power input switching

--- Firmware/CH32X035_Firmware/CH32X035_Firmware.ino
+++ Firmware/CH32X035_Firmware/CH32X035_Firmware.ino
@@ -6,6 +6,10 @@
 
 uint8_t setVoltage = REQUEST_20v;
 
+enum PowerSourceTypes {USBC, BARREL};
+
+PowerSourceTypes PowerSource;
+
 void PinInit(){
   pinMode(BUCK_EN, OUTPUT);
   digitalWrite(BUCK_EN, LOW);
@@ -19,7 +23,44 @@ void PinInit(){
   digitalWrite(LED_BUILTIN, LOW);
 }
 
+void processPowerSwap(PowerSourceTypes targetPowerSource){
+  if(targetPowerSource == USBC){
+    digitalWrite(BARREL_EN, LOW);
+    digitalWrite(PD_EN, HIGH);
+    PowerSource = USBC;
+    delay(200);
+  }
+  else{
+    digitalWrite(PD_EN, LOW);
+    digitalWrite(BARREL_EN, HIGH);
+    PowerSource = BARREL;
+    delay(200);
+  }
+}
+
+void delayWithPowerChecking(int delay_ms){
+  uint16_t PD_Voltage_measured;
+  uint16_t Barrel_Voltage_measured;
+  for(int counter = 0; counter < delay_ms; counter++){
+    PD_Voltage_measured = 1 + analogRead(PD_SENSE) >> 7;
+    if(PD_Voltage_measured == 8){
+        PD_Voltage_measured = 0x05; // when VBUS is 5V, VCC of CH32X035 is closer to 3.5V. This means that the analogRead is based on full scale of 3.5V, not 5V. This compensates for this affect.   
+    }
+
+    Barrel_Voltage_measured = analogRead(BARREL_SENSE);
+    if((PD_Voltage_measured < 3) && PowerSource == USBC){
+      processPowerSwap(BARREL);
+    }
+    else if((Barrel_Voltage_measured < 300) && PowerSource == BARREL){
+      processPowerSwap(USBC);
+    }
+    delay(1);
+  }
+}
+
 void setup() {
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
+  PWR_PVDLevelConfig(PWR_PVDLevel_4V0);
   PinInit();
   usbpd_sink_init();
   delay(100);
@@ -33,11 +74,13 @@ void setup() {
     }
   }
   delay(30); //just to be safe... 
-  if((analogRead(BARREL_SENSE)>>7) > 5){
+  if((analogRead(BARREL_SENSE)) > 300){
     digitalWrite(BARREL_EN, HIGH); //uses barrel power if barrel power is over 5V
+    PowerSource = BARREL;
   }
   else{
     digitalWrite(PD_EN, HIGH);
+    PowerSource = USBC;
   }
   delay(2); //determined experimentally: HSS transient over in 1ms
   digitalWrite(BUCK_EN, HIGH);
@@ -48,15 +91,32 @@ void loop() {
   if(usbpd_sink_get_ready()){
       usbpd_sink_set_request_fixed_voltage(setVoltage);
   }
-  uint8_t PD_Voltage = 1 + analogRead(PD_SENSE) >> 7; //Rev B01: 3.3K low, 18K high. CH32 has no divide instuction set, so we need to right shift to multiply / divide  
-  if(PD_Voltage == 8){
-    PD_Voltage = 0x05; // when VBUS is 5V, VCC of CH32X035 is closer to 3.5V. This means that the analogRead is based on full scale of 3.5V, not 5V. This compensates for this affect.   
+  if(PowerSource == USBC){
+    uint8_t InputVoltage;
+    if(PWR_GetFlagStatus(PWR_FLAG_PVDO)){
+      InputVoltage = 3;
+    }
+    else{
+      InputVoltage = 5;
+    }
+    float PD_Voltage = (float) analogRead(PD_SENSE) * SenseDividerRatio * InputVoltage / 4095; //Rev B01: 3.3K low, 18K high. CH32 has no divide instuction set, so we need to right shift to multiply / divide  
+    PD_Voltage = PD_Voltage * 1.12 - 0.2156; //fudge factors
+    if(PWR_GetFlagStatus(PWR_FLAG_PVDO)){
+      PD_Voltage = 0x05; // when VBUS is 5V, VCC of CH32X035 is closer to 3.5V. This means that the analogRead is based on full scale of 3.5V, not 5V. This compensates for this affect.   
+    }
+    for(uint8_t counter = 0; counter < (uint8_t) round(PD_Voltage); counter ++){
+      digitalWrite(LED_BUILTIN, HIGH);
+      delayWithPowerChecking(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delayWithPowerChecking(200);
+    }
+    delayWithPowerChecking(1000);
   }
-  for(uint8_t counter = 0; counter < PD_Voltage; counter ++){
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
+  else{
+      digitalWrite(LED_BUILTIN, HIGH);
+      delayWithPowerChecking(200);
+      digitalWrite(LED_BUILTIN, LOW);
+      delayWithPowerChecking(200);
   }
-  delay(1000);
+
 }
